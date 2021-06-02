d3703132509685ff9c339815b7c08860891443e4
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-06-02 16:25:31 +1000

    Add doco

--- src/Extensions/PassportExtension.php
+++ src/Extensions/PassportExtension.php
@@ -6,14 +6,22 @@ use SilverStripe\ORM\DataExtension;
 /**
  * Adds the OauthSource to the Identifier to avoid collisions when
  * multiple Oauth providers are used
+ *
+ * Adds unique indexes to the DB
  * @author James
  */
 class PassportExtension extends DataExtension {
 
+    /**
+     * @var array
+     */
     private static $db = [
         'OAuthSource' => 'Varchar(255)'
     ];
 
+    /**
+     * @var array
+     */
     private static $indexes = [
         'IdentifierProvider' => [
             'type' => 'unique',
