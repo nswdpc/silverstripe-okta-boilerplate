2c152f0f14122a78dc4f3ee5b7dd7be5722274ef
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-06-03 16:35:51 +1000

    Improve passport modeladmin experience, add created by for manually creaed passports, convert OAuthSource to a DropdownField to select the list of available providers,  add CED permission, save CreatedByMember when the record doesn't exist

--- src/Extensions/PassportExtension.php
+++ src/Extensions/PassportExtension.php
@@ -1,9 +1,14 @@
 <?php
 namespace NSWDPC\Authentication\Okta;
 
+use Bigfork\SilverStripeOAuth\Client\Factory\ProviderFactory;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\DropdownField;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Security\Member;
 use SilverStripe\Security\Permission;
 use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Security\Security;
 
 /**
  * Adds the OauthSource to the Identifier to avoid collisions when
@@ -19,6 +24,13 @@ class PassportExtension extends DataExtension implements PermissionProvider {
      */
     private static $db = [
         'OAuthSource' => 'Varchar(255)'
+    ];
+
+    /**
+     * @var array
+     */
+    private static $has_one = [
+        'CreatedByMember' => Member::class
     ];
 
     /**
@@ -42,7 +54,8 @@ class PassportExtension extends DataExtension implements PermissionProvider {
         'OAuthSource' => 'OAuth provider',
         'Member.Email' => 'Member',
         'Created' => 'Created',
-        'LastEdited' => 'Edited'
+        'LastEdited' => 'Edited',
+        'CreatedByMember.Email' => 'Created by'
     ];
 
     /**
@@ -53,6 +66,14 @@ class PassportExtension extends DataExtension implements PermissionProvider {
         'OAuthSource' => 'ExactMatchFilter',
         'Member.Email' => 'PartialMatchFilter'
     ];
+
+    public function onBeforeWrite() {
+        parent::onBeforeWrite();
+        if(!$this->owner->isInDB()) {
+            $member = Security::getCurrentUser();
+            $this->owner->CreatedByMemberID = $member->ID ?? 0;
+        }
+    }
 
     public function getTitle() {
         if($this->owner->exists()) {
@@ -73,21 +94,21 @@ class PassportExtension extends DataExtension implements PermissionProvider {
      * These fields are readonly and can only be deleted or viewed
      */
     public function canEdit($member) {
-        return false;
+        return Permission::checkMember($member, 'OAUTH_PASSPORT_EDIT');
     }
 
     /**
      * These fields are readonly and can only be deleted or viewed
      */
     public function canCreate($member) {
-        return false;
+        return Permission::checkMember($member, 'OAUTH_PASSPORT_EDIT');
     }
 
     /**
      * These fields are readonly and can only be deleted or viewed
      */
     public function canDelete($member) {
-        return Permission::checkMember($member, 'OAUTH_PASSPORT_DELETE');
+        return Permission::checkMember($member, 'OAUTH_PASSPORT_EDIT');
     }
 
     /**
@@ -102,8 +123,42 @@ class PassportExtension extends DataExtension implements PermissionProvider {
      */
     public function updateCmsFields($fields) {
         if($sourceField = $fields->dataFieldByName('OAuthSource')) {
-            $sourceField->setTitle(_t('OAUTH.SOURCE_TITLE', 'OAuth provider'));
+
+            $providerFactory = Injector::inst()->get(ProviderFactory::class);
+            $providers = $providerFactory->getProviders();
+            $listProviders = [];
+            $listProviders[ $this->owner->OAuthSource ] = _t(
+                'OKTA.PROVIDER_' . $this->owner->OAuthSource,
+                $this->owner->OAuthSource
+            );
+            if(is_array($providers)) {
+                foreach($providers as $providerName => $provider) {
+                    $listProviders[ $providerName ] = _t(
+                        'OKTA.PROVIDER_' . $providerName,
+                        $providerName
+                    );
+                }
+            }
+            $fields->replaceField(
+                'OAuthSource',
+                DropdownField::create(
+                    'OAuthSource',
+                    _t('OAUTH.SOURCE_TITLE', 'OAuth provider'),
+                    $listProviders,
+                    $this->owner->OAuthSource
+                )->setEmptyString('')
+            );
+
         }
+
+        if(!$this->owner->isInDB()) {
+            $fields->removeByName('CreatedByMemberID');
+        } else if($createdByMemberField = $fields->dataFieldByName('CreatedByMemberID')) {
+            $createdByMemberField->setTitle(_t('OAUTH.CREATED_BY_MEMBER', 'Created by'));
+            $fields->makeFieldReadonly('CreatedByMemberID');
+        }
+
+
     }
 
     /**
@@ -116,8 +171,8 @@ class PassportExtension extends DataExtension implements PermissionProvider {
                 'name' => _t('OAUTH.PERMISSION_VIEW', 'View OAuth passports'),
                 'category' => 'OAuth',
             ],
-            'OAUTH_PASSPORT_DELETE' => [
-                'name' => _t('OAUTH.PERMISSION_DELETE', 'Delete OAuth passports'),
+            'OAUTH_PASSPORT_EDIT' => [
+                'name' => _t('OAUTH.PERMISSION_CED', 'Create, edit and delete OAuth passports'),
                 'category' => 'OAuth',
             ]
         ];
