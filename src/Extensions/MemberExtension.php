6d5526df1b1b973c570a795106ff7429ed4fdcfa
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-06-11 15:23:29 +1000

    Provide a method to lock accounts out after a certain lapsed period of days where the account fails to sync

--- src/Extensions/MemberExtension.php
+++ src/Extensions/MemberExtension.php
@@ -4,6 +4,7 @@ namespace NSWDPC\Authentication\Okta;
 
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\ManyManyList;
+use SilverStripe\ORM\ValidationResult;
 use SilverStripe\Security\Member;
 use Symbiote\MultiValueField\Fields\KeyValueField;
 
@@ -36,6 +37,57 @@ class MemberExtension extends DataExtension
         );
         $fields->addFieldToTab('Root.Okta', $oktaProfileField);
         $fields->makeFieldReadonly('OktaProfile');
+        
+        if($oktaLastSyncField = $fields->dataFieldByName('OktaLastSync')) {
+            $fields->addFieldToTab('Root.Okta', $oktaLastSyncField);
+            $fields->makeFieldReadonly('OktaLastSync');
+        }
+            
+    }
+    
+    /**
+     * Extend {@link Member::validateCanLogin()} to block logins for anyone whose account has become stale
+     * @return void
+     */
+    public function canLogIn(ValidationResult &$result) {
+        
+        /**
+         * If the validation result is already a fail, go no further
+         */
+        if(!$result->isValid()) {
+            return false;
+        }
+        
+        $days = intval($this->owner->config()->get('okta_lockout_after_days'));
+        if($days <= 0) {
+            // if the configured days is 0 or less, OK
+            return true;
+        }
+        if(!$this->owner->OktaLastSync) {
+            // If the member has never been sync'd, allow
+            return;
+        }
+        
+        // calculate datetime comparison
+        try {
+            $dt = new \DateTime();
+            $odt = new \DateTime( $this->owner->OktaLastSync );
+            $odt->modify("+1 {$days} day");
+            if($odt < $dt) {
+                // still not on or after today
+                $result->addError(
+                    _t(
+                        'OKTA.ACCOUNT_TOO_OLD',
+                        'Sorry, you cannot sign in to this website as your account has not been used recently.'
+                        . ' Please contact a website administrator for further assistance.'
+                    )
+                );
+                return false;
+            }
+        } catch (\Exception $e) {
+            // noop
+        }
+        return true;
     }
 
     /**
